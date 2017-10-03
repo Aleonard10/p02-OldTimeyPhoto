98a1e52c7a78450f833bc4110873f926ac1e5cfd
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2017-10-02 17:21:39 -0700

    Commented in plan

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+  // get the photo and turn the pixels into a matrix
+  //loop that goes through every row in the matrix with a loop inside of it that goes through every column in the row
+  //average out the red, blue, and green for each input in row 0 within the row loop
+  //create a rgb with the average number as all 3 inputs before moving on to the next loop
+  //when cols hits its max, ++ rows and redo the loop
+  //continue until matrix completes and loops end
+  //allow user to save the greyscale photo
+  
+  return 0;
+}
